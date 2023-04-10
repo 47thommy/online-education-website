be9fcd32f5ca767df8e2f741a8f4359cbc118ad4
Thomas <47thommy@gmail.com>
2023-04-10 09:30:25 +0300

    done the  navigation

---
+++ script.js
@@ -0,0 +1,5 @@
+let menuBtn = document.querySelector("#menu-btn");
+let navBar = document.querySelector(".header .flex .navbar");
+menuBtn.onClick = () => {
+  menuBtn.classList.toggle("fa-times");
+};
